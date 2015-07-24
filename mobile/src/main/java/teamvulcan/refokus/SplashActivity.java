b7fd389db54577878463605b4bff70610e3e1d85
Nyctohylophobia <hotelkilocharlie@hotmail.sg>
2015-07-25 01:47:14 +0800

    Splash Screen added

---
+++ mobile/src/main/java/teamvulcan/refokus/SplashActivity.java
@@ -0,0 +1,42 @@
+package teamvulcan.refokus;
+
+import android.app.Activity;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+/**
+ * Created by kcheng.2013 on 24/7/2015.
+ */
+public class SplashActivity extends Activity {
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        StartAnimations();
+    }
+
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        LinearLayout l=(LinearLayout) findViewById(R.id.lin_lay);
+        l.clearAnimation();
+        l.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.logo);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+    }
+}
